a3ef88b3aee2cf2741eb2415862784e4b40b5868
Gurkan Sari <gurkansari1992@icloud.com>
2020-06-27 12:22:34 +0300

    initial xcode project creation

---
+++ youtube-onedaybuild/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube-onedaybuild
+//
+//  Created by Gürkan Sarı on 27.06.2020.
+//  Copyright © 2020 Gürkan Sarı. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
